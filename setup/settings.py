1fa445d4bdac14b22969ec9037c07db96d7457cd
LuizCenci <155825996+LuizCenci@users.noreply.github.com>
2024-10-09 11:05:04 -0300

    completed html and css

--- setup/settings.py
+++ setup/settings.py
@@ -138,9 +138,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'templates/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'templates/static')
+    os.path.join(BASE_DIR, 'templates/static/')
 ]
 
 # Default primary key field type
